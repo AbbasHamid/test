3705691921d95e798fcca85795a75e79676b4560
AbbasHamid <71654722+AbbasHamid@users.noreply.github.com>
2020-09-21 17:30:00 +0200

    Add files via upload

---
+++ A.java
@@ -0,0 +1,14 @@
+
+public class A {
+
+    String name;
+    int age;
+    String adress;
+
+    public void print() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(adress);
+    }
+
+}
